f5b770fca82973a9f50e4c2358e08914cdffbb47
xuexp <xuexp@cn.ibm.com>
2019-12-14 13:02:39 +0800

    pwchange

--- src/app/components/changepwd/changepwd.component.ts
+++ src/app/components/changepwd/changepwd.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient,HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 import { Router } from '@angular/router'; // 路由传参用到
-import { LocalURL } from '../../config/global-config';
-
-
-const BSEURL = LocalURL.serverURL + 'smcuser';
+import { Passwordcg } from './passwordcg';
+import { ChangepwdService } from './changepwd.service';
 
 @Component({
   selector: 'app-changepwd',
@@ -13,33 +11,35 @@ const BSEURL = LocalURL.serverURL + 'smcuser';
 })
 export class ChangepwdComponent implements OnInit {
 
-  oldpwd: string;
-  newpwd: string;
-  jsonData: Object;
-  confirm_newpwd: string;
-  constructor(private http: HttpClient,  private router: Router) { 
-    
+  passwordcg = new Passwordcg();
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private changepwdService: ChangepwdService
+    ) {}
+
+    submitNewPW() {
+      this.changepwdService.updatepw(this.passwordcg).subscribe(
+        res => {
+          if (res.data.msg === 200) {
+            alert(res.data.msg);
+            this.goTologinpage(res.data.msg);
+          } else {
+            alert(res.data.msg);
+          }
+        }, // success path
+        error => alert(error) // error path
+      );
   }
 
-  submitChangePwdRequest(){
-    console.log("token: ", "shazi "+ localStorage.getItem('JWT-Token'));
-    console.log("username",localStorage.getItem('currUserName'));
-    let headers = { headers: new HttpHeaders({ "Authorization": "shazi "+ localStorage.getItem('JWT-Token')})}
-    this.http.post(BSEURL + "/settings" 
-      ,{"username":localStorage.getItem('currUserName'),"password":this.oldpwd,"newpassword":this.newpwd},
-        headers )
-      .subscribe(res=>{
-        this.getFirstData(res)});
-  }
-
-  getFirstData(returnedObj: any){
-    alert(returnedObj.msg);
-    if(returnedObj.status==200){
-      this.router.navigateByUrl('/login');
-    }
+  goTologinpage(retrunMsg: string) {
+    alert(retrunMsg);
+    this.router.navigateByUrl('/login');
   }
 
   ngOnInit() {
+    this.passwordcg.username = localStorage.getItem('currUserName');
   }
-  
+
 }
