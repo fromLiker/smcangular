6ea3abcc597284059640fac30837458f08e1b7bc
xuexp <xuexp@cn.ibm.com>
2019-12-15 20:07:43 +0800

    prod url

--- src/app/config/global-config.ts
+++ src/app/config/global-config.ts
@@ -1,6 +1,5 @@
 export class LocalURL {
-    public static serverURL = 'http://localhost:8888/';
-    // public static serverURL = 'http://9.112.76.141:8888/';
+    // public static serverURL = 'http://localhost:8888/';
     // pro
-    // public static serverURL = 'http://106.54.194.33:8888/';
+    public static serverURL = 'http://106.54.194.33:8888/';
 }
